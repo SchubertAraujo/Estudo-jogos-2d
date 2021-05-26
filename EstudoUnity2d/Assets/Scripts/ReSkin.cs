f175ba9c15541d67d297933fadfb5efdb98a9388
SchubertAraujo <xubsoldier@gmail.com>
2021-05-26 15:11:16 -0300

    Adicionado personagens diferentes e armas diferentes

---
+++ EstudoUnity2d/Assets/Scripts/ReSkin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ReSkin : MonoBehaviour
+{
+    private SpriteRenderer sRender;
+
+    public Sprite[] sprites;
+    public string spriteSheetNames; //Nome do spritesheep que sera usado.
+    public string loadSpriteSheetNames;
+
+    private Dictionary<string, Sprite> spriteSheet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sRender = GetComponent<SpriteRenderer>();
+        LoadSpriteSheet();
+    }
+
+    // Update é chamado depois do update normal
+    void LateUpdate()
+    {
+        if(loadSpriteSheetNames != spriteSheetNames)
+        {
+            LoadSpriteSheet();
+        }
+
+        sRender.sprite = spriteSheet[sRender.sprite.name];
+
+    }
+
+    private void LoadSpriteSheet()
+    {
+        sprites = Resources.LoadAll<Sprite>(spriteSheetNames);
+        spriteSheet = sprites.ToDictionary(x => x.name, x => x);
+        loadSpriteSheetNames = spriteSheetNames;
+    }
+}
